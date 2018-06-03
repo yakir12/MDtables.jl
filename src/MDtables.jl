7996307317601e113246dfc5ff3e541ba586ddda
yakir12 <12.yakir@gmail.com>
2018-06-03 17:36:27 +0200

    added grided

--- src/MDtables.jl
+++ src/MDtables.jl
@@ -3,32 +3,55 @@ module MDtables
 
 using StringBuilders
 
-export printMD
+import Base:print
 
-fmtrfunc(x::String, s::Int) = x*" "^(s - length(x))
+export SimpleTable, MultilineTable, GridTable, print
 
-function printline!(o::T, x::Vector{String}, s::Int) where  {T<:StringBuilder}
-    for c in x[1:end-1]
-        append!(o, fmtrfunc(c, s))
-        append!(o, " ")
-    end
-    append!(o, fmtrfunc(x[end], s))
-    append!(o, "\n")
+abstract type MDtable end
+
+struct SimpleTable{T<:StringBuilder} <: MDtable
+    o::T
 end
 
-function printMD(a::Array)
-    o = StringBuilder()
-    nrow, ncol = size(a)
-    b = [[string(a[r,c]) for c in 1:ncol] for r in 1:nrow]
-    s = maximum(maximum(length, row) for row in b)
-    printline!(o, b[1], s)
-    printline!(o, fill("-"^s, ncol), s)
-    if nrow > 1 
-        for bi in b[2:end]
-            printline!(o, bi, s)
-        end
+struct MultilineTable{T<:StringBuilder} <: MDtable
+    o::T
+end
+
+struct GridTable{T<:StringBuilder} <: MDtable
+    o::T
+end
+
+function printline!(o, columns, widths, delimiter)
+    for (c, w) in zip(columns, widths)
+        append!(o.o, delimiter)
+        append!(o.o, rpad(c, w))
     end
-    return String(o)
+    append!(o.o, delimiter)
+    append!(o.o, "\n")
+end
+
+topbottom(o::SimpleTable{<:StringBuilder}, widths) = nothing
+topbottom(o::GridTable{<:StringBuilder}, widths) = printline!(o, "-".^widths, widths, "+")
+
+printrow(o::SimpleTable{<:StringBuilder}, columns, widths) = printline!(o, columns, widths, " ")
+printrow(o::GridTable{<:StringBuilder}, columns, widths) = printline!(o, columns, widths, "|")
+
+underline(o::SimpleTable{<:StringBuilder}, widths) = printline!(o, "-".^widths, widths, " ")
+underline(o::GridTable{<:StringBuilder}, widths) = printline!(o, "=".^widths, widths, "+")
+
+function print(::Type{T}, a::Array) where T <: MDtable
+    nrow, ncol = size(a)
+    rows = [[string(" ", a[r,c], " ") for c in 1:ncol] for r in 1:nrow]
+    widths = [maximum(length, getindex.(rows, col)) for col in 1:ncol]
+    o = T(StringBuilder())
+    topbottom(o, widths)
+    printrow(o, shift!(rows), widths)
+    underline(o, widths)
+    for row in rows
+        printrow(o, row, widths)
+    end
+    topbottom(o, widths)
+    return String(o.o)
 end
 
 end

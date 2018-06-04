bd92f617418bfab345160e63eae592f0d93bfb46
yakir12 <12.yakir@gmail.com>
2018-06-04 11:56:32 +0200

    multiline support

--- src/MDtables.jl
+++ src/MDtables.jl
@@ -1,8 +1,7 @@
 __precompile__()
 module MDtables
 
-using StringBuilders
-
+using StringBuilders, FixedSizeStrings
 import Base:print
 
 export SimpleTable, MultilineTable, GridTable, print
@@ -21,36 +20,73 @@ struct GridTable{T<:StringBuilder} <: MDtable
     o::T
 end
 
-function printline!(o, columns, widths, delimiter)
-    for (c, w) in zip(columns, widths)
-        append!(o.o, delimiter)
-        append!(o.o, rpad(c, w))
+struct Cell{H, W}
+    ss::NTuple{H, FixedSizeString{W}}
+end
+Cell(a::Vector{<:AbstractString}) = Cell((FixedSizeString.(a)...))
+function Cell(x::String) 
+    a = split(x, '\n')
+    h = maximum(length, a)
+    a .= rpad.(a, h)
+    Cell(a)
+end
+Cell(x) = Cell(string(x))
+geth(x::Cell{H,W}) where {H, W} = H
+getw(x::Cell{H,W}) where {H, W} = W
+pad(x::Cell{H, W}, h, w) where {H, W} = Cell([rpad.(x.ss, w)..., (" "^w for i in H+1:h)...])
+
+function printline(o, cells, l, delimiter)
+    for cell in cells
+        append!(o, delimiter)
+        append!(o, cell.ss[l])
     end
-    append!(o.o, delimiter)
-    append!(o.o, "\n")
+    append!(o, delimiter)
+    append!(o, "\n")
+end
+printline(o::SimpleTable, cells, l) = printline(o.o, cells, l, " ")
+printline(o::MultilineTable, cells, l) = printline(o.o, cells, l, " ")
+printline(o::GridTable, cells, l) = printline(o.o, cells, l, "|")
+printseparator(o, ws, c, d) = printline(o.o, [Cell((FixedSizeString(c^w), )) for w in ws], 1, d)
+printtopbottom(o::SimpleTable, ws) = nothing
+printtopbottom(o::MultilineTable, ws) = printseparator(o, ws, "-", "-")
+printtopbottom(o::GridTable, ws) = printseparator(o, ws, "-", "+")
+printdelimiter(o::SimpleTable, ws) = nothing
+printdelimiter(o::MultilineTable, ws) = printseparator(o, ws, " ", " ")
+printdelimiter(o::GridTable, ws) = printtopbottom(o, ws)
+printunderline(o::SimpleTable, ws) = printseparator(o, ws, "-", " ")
+printunderline(o::MultilineTable, ws) = printseparator(o, ws, "-", " ")
+printunderline(o::GridTable, ws) = printseparator(o, ws, "=", "+")
+function printrow(o, cells)
+    for l in 1:geth(cells[1])
+        printline(o, cells, l)
+    end
+end
+function getcells(a)
+    b = Cell.(a)
+    nrow, ncol = size(b)
+    hs = [maximum(geth, b[r, :]) for r in 1:nrow]
+    ws = [maximum(getw, b[:, c]) for c in 1:ncol]
+    for r in 1:nrow, c in 1:ncol
+        b[r, c] = pad(b[r, c], hs[r], ws[c])
+    end
+    return (b, ws, nrow)
 end
 
-topbottom(o::SimpleTable{<:StringBuilder}, widths) = nothing
-topbottom(o::GridTable{<:StringBuilder}, widths) = printline!(o, "-".^widths, widths, "+")
-
-printrow(o::SimpleTable{<:StringBuilder}, columns, widths) = printline!(o, columns, widths, " ")
-printrow(o::GridTable{<:StringBuilder}, columns, widths) = printline!(o, columns, widths, "|")
-
-underline(o::SimpleTable{<:StringBuilder}, widths) = printline!(o, "-".^widths, widths, " ")
-underline(o::GridTable{<:StringBuilder}, widths) = printline!(o, "=".^widths, widths, "+")
-
-function print(::Type{T}, a::Array) where T <: MDtable
-    nrow, ncol = size(a)
-    rows = [[string(" ", a[r,c], " ") for c in 1:ncol] for r in 1:nrow]
-    widths = [maximum(length, getindex.(rows, col)) for col in 1:ncol]
-    o = T(StringBuilder())
-    topbottom(o, widths)
-    printrow(o, shift!(rows), widths)
-    underline(o, widths)
-    for row in rows
-        printrow(o, row, widths)
+function print(::Type{T}, a::A) where {T <: MDtable, A <: AbstractArray}
+    b, ws, nrow = getcells(a)
+    if T <: SimpleTable
+        @assert all(geth(bi) == 1 for bi in b) "can't print multiline table as a simple_table"
     end
-    topbottom(o, widths)
+    o = T(StringBuilder())
+    printtopbottom(o, ws)
+    printrow(o, b[1, :])
+    printunderline(o, ws)
+    for r in 2:nrow-1
+        printrow(o, b[r, :])
+        printdelimiter(o, ws)
+    end
+    printrow(o, b[nrow, :])
+    printtopbottom(o, ws)
     return String(o.o)
 end
 
